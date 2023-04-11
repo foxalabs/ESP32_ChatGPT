34c883cb391f8ff0f532e0ee94d3b1886956547a
foxabilo <spencer.bentley@gmail.com>
2023-04-11 18:53:41 +0100

    First Commit

---
+++ src/ESP32_ChatGPT.cpp
@@ -0,0 +1,88 @@
+#include "ESP32_ChatGPT.h"
+
+ChatGPT::ChatGPT(const char* apiKey, const char* rootCA): _apiKey(apiKey) {
+    _client.setCACert(rootCA);
+}
+
+String ChatGPT::createCompletion(const JsonArray& messages,
+    const String& model,
+    unsigned long timeout,
+    int maxTokens,
+    float temperature,
+    float top_p,
+    int n,
+    bool stream,
+    const String& stop,
+    float presence_penalty,
+    float frequency_penalty,
+    const JsonObject& logit_bias,
+    const String& user) {
+    if (!_client.connect(_apiDomain, 443)) {
+        Serial.println("Connection failed");
+        return "";
+    }
+
+    DynamicJsonDocument jsonDoc(2048);
+    JsonObject root = jsonDoc.to<JsonObject>();
+    root["model"] = model;
+    root["messages"] = messages;
+    root["temperature"] = temperature;
+    root["top_p"] = top_p;
+    root["n"] = n;
+    root["stream"] = stream;
+    if (maxTokens != -1) {
+        root["max_tokens"] = maxTokens;
+    }
+    if (stop.length() > 0) {
+        root["stop"] = stop;
+    }
+    root["presence_penalty"] = presence_penalty;
+    root["frequency_penalty"] = frequency_penalty;
+    if (!logit_bias.isNull()) {
+        root["logit_bias"] = logit_bias;
+    }
+    if (user.length() > 0) {
+        root["user"] = user;
+    }
+
+    String payload;
+    serializeJson(root, payload);
+
+    String authorizationHeader = "Authorization: Bearer " + String(_apiKey);
+    //Serial.println(authorizationHeader);
+    _client.println("POST " + String(_apiPath) + " HTTP/1.1");
+    _client.println("Host: " + String(_apiDomain));
+    _client.println("Content-Type: application/json");
+    _client.println(authorizationHeader);
+    _client.println("Content-Length: " + String(payload.length()));
+    _client.println();
+    _client.print(payload);
+
+    String response = "";
+    bool header_passed = false;
+
+    //unsigned long timeout = 5000; // 5 seconds timeout
+    unsigned long start = millis();
+
+    while (_client.connected() && (millis() - start < timeout)) {
+        if (_client.available()) {
+            String line = _client.readStringUntil('\n');
+
+            if (!header_passed && line == "\r") {
+                header_passed = true;
+            }
+            else if (header_passed) {
+                response += line;
+                if (response.indexOf("\"finish_reason\"") != -1) { // Check for the finish_reason field in the response
+                    break;
+                }
+            }
+        }
+    }
+
+    StaticJsonDocument<2048> jsonResponse;
+    deserializeJson(jsonResponse, response);
+    String chatResponse = jsonResponse["choices"][0]["message"]["content"].as<String>();
+
+    return chatResponse;
+}
---
+++ src/ESP32_ChatGPT.h
@@ -0,0 +1,34 @@
+#ifndef ESP32_CHATGPT_H
+#define ESP32_CHATGPT_H
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <ArduinoJson.h>
+
+class ChatGPT {
+public:
+  ChatGPT(const char* apiKey, const char* rootCA);
+  String createCompletion(
+    const JsonArray& messages, 
+    const String& model, 
+    unsigned long timeout = 5000,
+    int maxTokens = -1, 
+    float temperature = 1, 
+    float top_p = 1, 
+    int n = 1, 
+    bool stream = false, 
+    const String& stop = "", 
+    float presence_penalty = 0, 
+    float frequency_penalty = 0, 
+    const JsonObject& logit_bias = JsonObject(), 
+    const String& user = "");
+
+private:
+  const char* _apiKey;
+  const char* _apiDomain = "api.openai.com";
+  const char* _apiPath = "/v1/chat/completions";
+  WiFiClientSecure _client;
+};
+
+#endif // ESP32_CHATGPT_H
