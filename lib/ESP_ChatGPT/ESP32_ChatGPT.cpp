3e8c9e9eb229cd3ab32e366227b16f12403b793e
Spencer Bentley <spencer.bentley@gmail.com>
2023-04-13 22:36:51 +0100

    Added Apache License Info

--- lib/ESP_ChatGPT/ESP32_ChatGPT.cpp
+++ lib/ESP_ChatGPT/ESP32_ChatGPT.cpp
@@ -1,10 +1,17 @@
-/**************************************************************************
- *                                                                        *
- *  Author: Spencer Bentley                                               *
- *  Date: 2023-04-11                                                      *
- *  Description: [ESP32_ChatGPT.cpp Library]                              *
- *                                                                        *
- **************************************************************************/
+ /*Copyright [2023-04-11] [Spencer Bentley]
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License. */
+
 #define JsonMaxSize 4096
 #include "ESP32_ChatGPT.h"
 #include <WiFiClientSecure.h>
